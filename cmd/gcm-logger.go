111133b3ef2b643c1abe22cb92c0039345e8a141
Ali Afshar <afshar@google.com>
2015-07-30 03:29:38 +0100

    Added command line script for easy testing of GCM clients round trip.

--- cmd/gcm-logger.go
+++ cmd/gcm-logger.go
@@ -1,34 +1,34 @@
 // Program gcm-logger logs and echoes as a GCM "server".
 package main
 
-import(
-  "github.com/google/go-gcm"
-  "github.com/aliafshar/toylog"
-  "github.com/alecthomas/kingpin"
+import (
+	"github.com/alecthomas/kingpin"
+	"github.com/aliafshar/toylog"
+	"github.com/google/go-gcm"
 )
 
-var(
-  serverKey = kingpin.Flag("server_key", "The server key to use for GCM.").Short('k').Required().String()
-  senderId = kingpin.Flag("sender_id", "The sender ID to use for GCM.").Short('s').Required().String()
+var (
+	serverKey = kingpin.Flag("server_key", "The server key to use for GCM.").Short('k').Required().String()
+	senderId  = kingpin.Flag("sender_id", "The sender ID to use for GCM.").Short('s').Required().String()
 )
 
 // onMessage receives messages, logs them, and echoes a response.
 func onMessage(from string, d gcm.Data) error {
-  toylog.Infoln("Message, from:", from, "with:", d)
-  // Echo the message with a tag.
-  d["echoed"] = true
-  m := gcm.HttpMessage{To: from, Data: d}
-  r, err := gcm.SendHttp(*serverKey, m)
-  if err != nil {
-    toylog.Errorln("Error sending message.", err)
-    return err
-  }
-  toylog.Infof("Sent message. %+v -> %+v", m, r)
-  return nil
+	toylog.Infoln("Message, from:", from, "with:", d)
+	// Echo the message with a tag.
+	d["echoed"] = true
+	m := gcm.HttpMessage{To: from, Data: d}
+	r, err := gcm.SendHttp(*serverKey, m)
+	if err != nil {
+		toylog.Errorln("Error sending message.", err)
+		return err
+	}
+	toylog.Infof("Sent message. %+v -> %+v", m, r)
+	return nil
 }
 
 func main() {
-  toylog.Infoln("GCM Logger, starting.")
-  kingpin.Parse()
-  gcm.Listen(*senderId, *serverKey, onMessage, nil)
+	toylog.Infoln("GCM Logger, starting.")
+	kingpin.Parse()
+	gcm.Listen(*senderId, *serverKey, onMessage, nil)
 }
