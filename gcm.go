b9565a0ee48a6f5e80ab383a8572c7b185d303eb
Hugo Vieira <hugo.andre.vieira@gmail.com>
2017-08-23 14:56:59 +0100

    Added android channel id to notification struct

--- gcm.go
+++ gcm.go
@@ -116,6 +116,7 @@ type Notification struct {
 	Tag          string `json:"tag,omitempty"`
 	Color        string `json:"color,omitempty"`
 	ClickAction  string `json:"click_action,omitempty"`
+	AndroidChID  string `json:"android_channel_id"`
 	BodyLocKey   string `json:"body_loc_key,omitempty"`
 	BodyLocArgs  string `json:"body_loc_args,omitempty"`
 	TitleLocArgs string `json:"title_loc_args,omitempty"`
